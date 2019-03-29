9bb0d17707a9797bf4e020a67cadadbd9e5f0ceb
Jonathan Hernandez <jon.hernandez169@gmail.com>
2019-03-28 22:36:50 -0500

    Updates

---
+++ precache-manifest.0b095536b91e5b80c68e19d13fd790d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "45e9a843ff2a08515a23",
+    "url": "/static/js/main.807d467c.chunk.js"
+  },
+  {
+    "revision": "0622e8c1bb039ac44a66",
+    "url": "/static/js/2.ff00731d.chunk.js"
+  },
+  {
+    "revision": "45e9a843ff2a08515a23",
+    "url": "/static/css/main.cff60905.chunk.css"
+  },
+  {
+    "revision": "abb8532555e5e9dd18e06074b7c60ca6",
+    "url": "/index.html"
+  }
+];
